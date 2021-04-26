bf24b980e6c84094231e808cb41845a8b6db0250
Infernus <76605774+ankitrana1256@users.noreply.github.com>
2021-04-27 00:13:01 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,13 @@
+# import turtle library
+import turtle             
+#col = [ "red","purple","blue","green","orange","yellow"]
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.bgcolor('black')
+t.speed(25)
+for i in range(150):
+    t.color('white')
+    t.forward(i*4)
+    t.right(144)
+    t.width(1)
+    
